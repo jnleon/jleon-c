418c46eb75c85d316fb31951eba5f791e794d10d
Katie Dwyer <katie.dwyer@techelevator.com>
2020-09-16 12:05:01 -0400

    03 class lecture and exercise

---
+++ module-1/03_Expressions/lecture-final/dotnet/Lecture/12_ReturnFizzIfThreeUsingTernary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    public partial class LectureExample
+    {
+
+        /*
+        12. Now write the above using the Ternary operator ?:
+        */
+        public string ReturnFizzIfThreeUsingTernary(int number)
+        {
+            //ternary: (conditional)? <if true> : <if false>
+            return (number == 3) ? "Fizz" : ""; 
+        }
+    }
+}

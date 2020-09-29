4ce2a44eed8c2fee1661deee89d14cb70b3d65d0
Katie Dwyer <katie.dwyer@techelevator.com>
2020-09-29 11:13:06 -0400

    m1 12 class lecture and exercise

---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Elephant.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Elephant : FarmAnimal, ISellable
+    {
+        decimal _price;
+        public decimal Price { 
+            get
+            {
+                return _price;
+            }
+       }
+
+        //decimal ISellable.Price => throw new NotImplementedException();
+
+        public Elephant() : base("ELEPHANT")
+        {
+            _price = 5000.50M; 
+        }
+
+        public override string MakeSoundOnce()
+        {
+            return "TRUMPET";
+        }
+
+        public override string MakeSoundTwice()
+        {
+            return "TRUMPET TRUMPET";
+        }
+    }
+}
---
+++ module-1/12_Polymorphism/lecture-final/dotnet/Lecture/Farming/Snake.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Snake : FarmAnimal
+    {
+        public Snake() : base("SNAKE")
+        { 
+        }
+
+        public override string MakeSoundOnce()
+        {
+            return "SSSSSS";
+        }
+
+
+        public override string MakeSoundTwice()
+        {
+            return "SSSSSS SSSSSS";
+        }
+
+        public void Squish()
+        {
+
+        }
+    }
+}

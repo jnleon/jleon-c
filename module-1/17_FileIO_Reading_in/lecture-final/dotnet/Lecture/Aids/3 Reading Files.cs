58b686ac18da3a9397081f8468f3ce74e688935c
Katie Dwyer <katie.dwyer@techelevator.com>
2020-10-05 12:36:02 -0400

    m1 17 file read lecture and exercise

---
+++ module-1/17_FileIO_Reading_in/lecture-final/dotnet/Lecture/Aids/3 Reading Files.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture.Aids
+{
+    /*
+    * Reading files for input involves working with streams and readers.
+    */ 
+    public static class ReadingInFiles
+    {
+
+        // Reading in a character file involves working with classes that derive from 
+        // TextWriter. TextWriter is an abstract class for working with character input.
+        // The StreamReader inherits from TextWriter and that is often used.
+        public static void ReadACharacterFile()
+        {
+            // Start with the file path to input
+            string directory = Environment.CurrentDirectory;
+            string filename = "Input.txt";
+
+            // Create the full path
+            string fullPath = Path.Combine(directory, filename);
+            try
+            {
+
+                //i can use a StreamReader without the using but then I have to remember to close it
+                StreamReader bla = new StreamReader("C:\\tests.txt");
+                bla.Close();
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Katie's example of creating a StreamReader without using using tries to read a file that doesn't exist.");
+            }
+
+            // Wrap the effort in a try-catch block to handle any exceptions
+            try
+            {
+                //Open a StreamReader with the using statement
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    // Read the file until the end of the stream is reached
+                    // EndOfStream is a "marker" that the stream uses to determine 
+                    // if it has reached the end
+                    // As we read forward the marker moves forward like a typewriter.
+                    while (!sr.EndOfStream)
+                    {
+                        // Read in the line
+                        string line = sr.ReadLine();
+                        
+                        // Print it to the screen
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch(IOException e) //catch a specific type of Exception
+            {
+                Console.WriteLine("Error reading the file");
+                Console.WriteLine(e.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ module-1/17_FileIO_Reading_in/lecture-final/dotnet/Lecture/Program.cs
@@ -0,0 +1,61 @@
+﻿using Lecture.Aids;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //FileAndDirectories.UsingTheDirectoryClass();
+            //FileAndDirectories.UsingTheFileClass();
+            //ReadingInFiles.ReadACharacterFile();
+            //ReadingCSVFiles.ReadFile();
+            //SummingUpNumbers.ReadFile();         
+            ReadFileHamletAndOutputWithKatieAsFRANCISCO();
+        }
+
+        private static void ReadFileHamletAndOutputWithKatieAsFRANCISCO()
+        {
+           
+            // Start with the file path to input
+            string directory = Environment.CurrentDirectory;
+            string filename = "Hamlet.txt";
+
+            // Create the full path
+            string fullPath = Path.Combine(directory, filename);
+
+            string fullPathAbsolute = "C:\\Users\\Student\\example.txt"; //ABSOLUTE PATH
+
+            try 
+            {
+                //open the file for reading
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    while (!sr.EndOfStream) //until i've reached the end of the file
+                    {
+                        //read each line
+                        string line = sr.ReadLine();
+
+                        //if the line contains FRANCISCO, replace it with KATIE
+                        string newLine = line.Replace("FRANCISCO", "KATIE");
+
+                        // then output to the console
+                        Console.WriteLine(newLine);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("oh no! " + e.Message);
+            }
+
+
+        }
+    }
+}

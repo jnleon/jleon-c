fd780b145c47c97551f1464a6c5719780f4d2700
Katie Dwyer <katie.dwyer@techelevator.com>
2020-10-06 11:29:53 -0400

    m1 18 File Write

---
+++ module-1/18_FileIO_Writing_out/lecture-final/dotnet/Lecture/Aids/5 Writing TextFiles.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture.Aids
+{
+    public static class WritingTextFiles
+    {
+        /*
+        * This method below provides sample code for printing out a message to a text file.
+        */
+        public static void WritingAFile()
+        {
+            string directory = Environment.CurrentDirectory;
+            string filename = "output.txt";
+            string fullPath = Path.Combine(directory, filename);
+
+            using(StreamWriter sw = new StreamWriter(fullPath,true)) //if the file doesn't exist, create it. If it does exist, open for append
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("World");
+
+                sw.Write("these ");
+                sw.Write("will ");
+                sw.Write(" be on the same line");
+
+            }
+
+            //using is a much better idean than the next 4 lines
+            StreamWriter sw2 = new StreamWriter(fullPath, true);
+            sw2.Write("bla");
+            sw2.Flush(); //writes everything buffering in memory to the actual file
+            sw2.Close(); // flush and close
+
+            // After the using statement ends, file has now been written
+            // and closed for further writing
+        }
+
+        public static void WritingAFileWithNoPath()
+        {
+
+            string fullPath = "putThisFileIntheDefaultpath.txt";
+
+            using (StreamWriter sw = new StreamWriter(fullPath, false)) //if the file doesn't exist, create it. If it does exist, open for overwrite
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("World");
+
+                sw.Flush(); //very few cases where you should actually do this
+
+                sw.Write("these ");
+                sw.Write("will ");
+                sw.Write(" be on the same line");
+
+            }
+
+            //using is a much better idean than the next 4 lines
+            StreamWriter sw2 = new StreamWriter(fullPath, true);
+            sw2.Write("bla");
+            sw2.Flush(); //writes everything buffering in memory to the actual file
+            sw2.Close(); // flush and close
+
+            // After the using statement ends, file has now been written
+            // and closed for further writing
+        }
+
+
+
+    }
+}
---
+++ module-1/18_FileIO_Writing_out/lecture-final/dotnet/Lecture/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using Lecture.Aids;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regular = "this\nis\na\nstring\twith\nescape\ncharacters";
+            string withAtSign = @"this\nis\na\nstring\twith\nescape\ncharacters";
+
+            string path = @"C:\Users\Katie Dwyer\workspace\c-base\module-1\18_FileIO_Writing_out\lecture-final\dotnet\Lecture\bin\Debug\netcoreapp2.2";
+
+            Console.WriteLine("Regular string: " + regular);
+            Console.WriteLine("With @ sign: " + withAtSign);
+
+            //HardCodePath();
+
+            //FileAndDirectories.UsingTheDirectoryClass();
+
+            WritingTextFiles.WritingAFile();
+            WritingTextFiles.WritingAFileWithNoPath();
+
+            PerformanceDemo.SlowPerformance();
+            PerformanceDemo.FastPerformance();
+
+            LoopingCollectionToWriteFile.LoopingADictionaryToWriteAFile();
+            ReadingAndWritingFiles.OpenAndWrite();
+            BinaryImageManipulator.ReadFileIn();
+
+            Console.Write("Press enter to finish");
+            Console.ReadLine();
+        }
+
+        private static void HardCodePath()
+        {
+            string path = @"C:\Users\Katie Dwyer\AssetPanda";
+            string fullPathToFile = Path.Combine(path, "Cohort9.csv"); 
+            try
+            {
+                using(StreamReader sr = new StreamReader(fullPathToFile))
+                {
+                    //only read the first 3 lines
+                    int lineNum = 0;
+                    while(!sr.EndOfStream && lineNum<3)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                        lineNum++;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("oops {0}", e.Message);
+            }
+        }
+    }
+}

4d200929215677b6c3fd90967beaf1b75ce368ee
Juan Leon <juanix131@hotmail.com>
2020-10-05 22:21:36 -0400

    FILE IO PART 1 HW

--- module-1/17_FileIO_Reading_in/student-exercise/dotnet/WordSearch/Program.cs
+++ module-1/17_FileIO_Reading_in/student-exercise/dotnet/WordSearch/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WordSearch
 {
@@ -6,11 +7,54 @@ namespace WordSearch
     {
         static void Main(string[] args)
         {
+            //C:\Users\Student\workspace\jleon-c\module-1\17_FileIO_Reading_in\student-exercise\dotnet\alices_adventures_in_wonderland.txt
+
             //1. Ask the user for the search string
+            Console.WriteLine("What is fully qualified name of the file that should be searched?");
+            string fullPath = Console.ReadLine();
+            int lineNumber = 0;
+
             //2. Ask the user for the file path
-            //3. Open the file
-            //4. Loop through each line in the file
-            //5. If the line contains the search string, print it out along with its line number
+            Console.WriteLine("What is the search word you are looking for?");
+            string findWord = Console.ReadLine();
+
+            //3. QUESTION
+            Console.WriteLine(@"Should the search be case sensitive? (Y\N)");
+            string yesorNo = Console.ReadLine();
+            
+            if (yesorNo == "Y" || yesorNo == "y")
+            {
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        lineNumber++;
+
+                        //5. If the line contains the search string, print it out along with its line number
+                        if (line.Contains(findWord))
+                        {
+                            Console.WriteLine(lineNumber + ") " + line);
+                        }
+                    }
+                }
+            }
+            else
+            using (StreamReader sr1 = new StreamReader(fullPath))
+            {
+                while (!sr1.EndOfStream)
+                {
+                    string line = sr1.ReadLine ();
+                    
+                    lineNumber++;
+
+                    //5. If the line contains the search string, print it out along with its line number
+                        if (line.ToLower().Contains(findWord.ToLower()))
+                    {
+                        Console.WriteLine(lineNumber + ") " + line);
+                    }
+                }
+            }
         }
     }
 }
--- module-1/17_FileIO_Reading_in/student-exercise/dotnet/file-io-part1-exercises/Program.cs
+++ module-1/17_FileIO_Reading_in/student-exercise/dotnet/file-io-part1-exercises/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace file_io_part1_exercises
 {
@@ -6,7 +8,66 @@ namespace file_io_part1_exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+C:\Users\Student\workspace\jleon-c\module-1\17_FileIO_Reading_in\student-exercise\dotnet\sample-quiz-file.txt
+
+C:\Users\Student\workspace\jleon-c\module-1\17_FileIO_Reading_in\student-exercise\dotnet\another_quiz.txt
+             */
+
+            Console.WriteLine("Enter the fully qualified name of the file to read in for quiz questions");
+            string filePath = Console.ReadLine();
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                int correctOnes = 0;
+                int numberQuestions = 0;
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] splitLine = line.Split("|");
+                    string question = (splitLine[0]);
+                    int correctAnswer = 0;
+
+                    List<string> answer1 = new List<string>();
+
+                    for (int i = 1; i < splitLine.Length; i++)
+                    {
+                        answer1.Add(splitLine[i]);
+                    }
+
+                    Console.WriteLine(question);
+
+                    for (int i = 0; i < answer1.Count; i++)
+                    {
+                        if (answer1[i].EndsWith("*"))
+                        {
+                            Console.WriteLine(i + 1 + ". " + answer1[i].Substring(0, splitLine.Length - 1));
+                            continue;
+                        }
+                        Console.WriteLine(i + 1 + ". " + answer1[i]);
+                    }
+
+                    correctAnswer = answer1.FindIndex(x => x.EndsWith("*")) + 1;
+
+                    Console.WriteLine("Your answer:");
+                    int hello = Convert.ToInt32(Console.ReadLine());
+
+                    if (hello == correctAnswer)
+                    {
+                        numberQuestions++;
+                        correctOnes++;
+                        Console.WriteLine("RIGHT!");
+                    }
+                    else
+                    {
+                        numberQuestions++;
+                        Console.WriteLine("WRONG!");
+                    }
+                    Console.WriteLine("You got {0} answer(s) correct out of the {1} questions asked.", correctOnes, numberQuestions);
+                }
+            }
         }
     }
 }
+

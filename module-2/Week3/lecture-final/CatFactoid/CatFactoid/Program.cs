10d235d55b72de93617e1f56c4a34870dd9634c2
Katie Dwyer <katie.dwyer@techelevator.com>
2020-10-30 12:02:05 -0400

    m2 week 3 review

---
+++ module-2/Week3/lecture-final/CatFactoid/CatFactoid/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CatFactoid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApiService api = new ApiService("https://catfact.ninja/fact");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press 0 to exit, anything else for another cat fact");
+            string input = Console.ReadLine();
+            while(input!="0")
+            {
+                //show a catfact
+                Console.WriteLine(api.GetRandomCatFact());
+
+                //prompt again
+                Console.WriteLine("Press 0 to exit, anything else for another cat fact");
+                input = Console.ReadLine();
+
+            }
+        }
+    }
+}

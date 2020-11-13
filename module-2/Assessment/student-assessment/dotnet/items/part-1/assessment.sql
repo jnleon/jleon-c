f396f2a072cac912025cd97338adc82f90558290
Juan Leon <juanix131@hotmail.com>
2020-11-13 10:13:51 -0500

    assesment

--- module-2/Assessment/student-assessment/dotnet/items/part-1/assessment.sql
+++ module-2/Assessment/student-assessment/dotnet/items/part-1/assessment.sql
@@ -2,11 +2,17 @@ USE assessment;
 GO
 
 -- Select all columns from users where the user's role is admin
-
+select * from users where users.role='admin';
 -- Select name and description from items that were created after Sept. 20, 2019 and the description isn't null
-
+select name, description from items where items.created = '2019-09-20' AND description IS NOT NULL;
 -- Select first_name and last_name from users and order by when they were created, latest first
-
+select first_name, last_name from users ORDER BY created desc;
 -- Select finished and a count all the items that are finished/not finished
-
+select COUNT(*) AS 'items finished' from items where finished=0; 
+select COUNT(*) AS 'items not finished' from items where finished=1;
 -- Select a user's first_name and last_name and the item's name for every finished item
+select u.first_name, u.last_name, name from items i
+INNER JOIN users u ON i.id = u.id
+WHERE i.finished =1;
+
+
--- module-2/Assessment/student-assessment/dotnet/items/part-2/Items/Items/UserSqlDao.cs
+++ module-2/Assessment/student-assessment/dotnet/items/part-2/Items/Items/UserSqlDao.cs
@@ -15,14 +15,65 @@ namespace Items
 
         public IList<User> GetAllUsers()
         {
-            // Implement this method to pull in all users from database
+            List<User> list = new List<User>();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * from users", conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        User user = new User();
+                        user.FirstName = Convert.ToString(reader["first_name"]);
+                        user.LastName = Convert.ToString(reader["last_name"]);
+                        user.Email = Convert.ToString(reader["email"]);
+                        user.Role = Convert.ToString(reader["role"]);
+                        user.Created = Convert.ToDateTime(reader["created"]);
 
-            return null;
+
+                        list.Add(user);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error getting users.");
+                Console.WriteLine(e.Message);
+                throw;
+            }
+            return list;
         }
 
         public void Save(User newUser)
         {
-            // Implement this method to save user to database
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO users(first_name,last_name,email,role,created)" +
+                                                   " VALUES(@first_name,@last_name,@email,@role,@created)", conn);
+                    
+                    cmd.Parameters.AddWithValue("@first_name", newUser.FirstName);
+                    cmd.Parameters.AddWithValue("@last_name", newUser.LastName);
+                    cmd.Parameters.AddWithValue("@email", newUser.Email);
+                    cmd.Parameters.AddWithValue("@role", newUser.Role);
+                    cmd.Parameters.AddWithValue("@created", newUser.Created) ;
+
+ 
+                    int numRowsAffected = cmd.ExecuteNonQuery();
+                    Console.WriteLine("Number of rows affected: " + numRowsAffected);
+
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error getting users.");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
-}
+    }

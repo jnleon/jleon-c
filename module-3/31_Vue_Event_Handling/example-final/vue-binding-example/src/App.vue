b10bb8595afac931fcccc3b036a3690f32812c5c
Katie Dwyer <katie.dwyer@techelevator.com>
2020-11-24 11:51:42 -0500

    vue event lecture and exercise'

---
+++ module-3/31_Vue_Event_Handling/example-final/vue-binding-example/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+  
+    <data-binding-example />
+  </div>
+</template>
+
+<script>
+import DataBindingExample from './components/DataBindingExample.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DataBindingExample
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
